1cc059378f0d9fc9ac8213148697fd8d8be0222b
Harsh <harshrastogi396@gmail.com>
2024-02-05 13:07:52 +0530

    first commit

--- src/app/SignUp/page.tsx
+++ src/app/SignUp/page.tsx
@@ -1,9 +1,10 @@
+import SignUp from '@/components/SignUpPageComponents/SignUp'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      hello
+      <SignUp/>
     </div>
   )
 }
